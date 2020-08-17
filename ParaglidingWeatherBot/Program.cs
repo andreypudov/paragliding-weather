ffea6f2a4c1baa6407046533cd52f49af06a6b71
Andrey Pudov <andrey.pudov@intel.com>
2020-08-18 02:07:54 +0300

    ~ added wind gusts, precipitation and humidity

--- ParaglidingWeatherBot/Program.cs
+++ ParaglidingWeatherBot/Program.cs
@@ -36,6 +36,8 @@ namespace ParaglidingWeatherBot
                 return;
             }
 
+            Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
             switch (e.Message.Text.Trim())
             {
                 case "/forecast":
@@ -58,21 +60,42 @@ namespace ParaglidingWeatherBot
             var temperatures = document.DocumentNode.SelectNodes("(//table[@id='forecast']//td[contains(@class, '213')])");
             var wind_dirs = document.DocumentNode.SelectNodes("(//table[@id='forecast']//td[contains(@class, 'wind_dir ')])");
             var winds = document.DocumentNode.SelectNodes("(//table[@id='forecast']//td[contains(@class, 'wind ')])");
+            var wind_gusts = document.DocumentNode.SelectNodes("(//table[@id='forecast']//td[contains(@class, '332 ')])");
+            var precipitation = document.DocumentNode.SelectNodes("(//table[@id='forecast']//td[contains(@class, '222 ')])");
+            var humidity = document.DocumentNode.SelectNodes("(//table[@id='forecast']//td[contains(@class, '215 ')])");
 
             var buidler = new StringBuilder();
+            int hour = 0;
+            int today = DateTime.Today.Day;
 
+            buidler.Append("<pre>").Append(today).Append('\n');
             for (var index = 0; index < hours.Count; ++index)
             {
+                if (hour == 8)
+                {
+                    hour = 0;
+                    today = today + 1;
+                    buidler.Append('\n').Append(today).Append('\n');
+                }
+
                 buidler
-                    .Append(hours[index].InnerText.Trim()).Append(", ")
-                    .Append(temperatures[index].InnerText.Trim()).Append(", ")
-                    .Append(wind_dirs[index].InnerText.Trim()).Append(", ")
-                    .Append(winds[index].InnerText.Trim()).Append('\n');
+                    .Append($"{hours[index].InnerText.Trim(),-4}, ")
+                    .Append($"{temperatures[index].InnerText.Trim(),4}, ")
+                    .Append($"{wind_dirs[index].InnerText.Trim().ToUpperInvariant(),4}, ")
+                    .Append($"{winds[index].InnerText.Trim(),3}, ")
+                    .Append($"{wind_gusts[index].InnerText.Trim(),3}, ")
+                    .Append($"{humidity[index].InnerText.Trim(),3}, ")
+                    .Append($"{precipitation[index].InnerText.Trim(),4}\n");
+
+                hour = hour + 1;
             }
+
+            buidler.Append("</pre>");
 
             await botClient.SendTextMessageAsync(
                         chatId: e.Message.Chat,
-                        text: buidler.ToString()).ConfigureAwait(false);
+                        text: buidler.ToString(),
+                        parseMode: Telegram.Bot.Types.Enums.ParseMode.Html).ConfigureAwait(false);
         }
     }
 }
